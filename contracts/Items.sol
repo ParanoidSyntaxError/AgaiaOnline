2551671743a66b5319c46714cf14f2e6399f883e
ParanoidSyntaxError <git@jbojtor.com>
2022-10-21 21:57:09 +1100

    Item refactor

--- contracts/Items.sol
+++ contracts/Items.sol
@@ -14,12 +14,10 @@ contract Items is ItemsInterface, ERC1155, Ownable {
         string value;
     }
 
-    // Address => ID => Balance
-    mapping(address => mapping(uint256 => uint256)) internal _totalBalances;
-
-    // ID => Supply
-    mapping(uint256 => uint256) internal _totalSupplys;
-    uint256 internal _supply;
+    struct Item {
+        string name;
+        uint256 svgHash;
+    }
 
     // Index => SVG rectangles hash
     mapping(uint256 => Attribute) internal _bases;
@@ -28,6 +26,14 @@ contract Items is ItemsInterface, ERC1155, Ownable {
     // Index => SVG effect
     mapping(uint256 => Attribute) internal _effects;
     uint256 internal _totalEffects;
+
+    // ID => Item
+    mapping(uint256 => Item) _items;
+    uint256 internal _totalItems;
+
+    // ID => Supply
+    mapping(uint256 => uint256) internal _totalSupplys;
+    uint256 internal _totalSupply;
 
     constructor() ERC1155("") {
         // Initial bases
@@ -39,9 +45,16 @@ contract Items is ItemsInterface, ERC1155, Ownable {
             "00002404002024040004061618040616060405011304050106050402140504021105020206070501130705010608040114080401060902011609020106100102171001021009040108100201081101011410020115110101111101041214030116120104151601021417010110180401081702010816010107120104141904010619040106180201061601021618020117160102");
         _bases[3] = Attribute("Cracked Orb", 
             "000024060018240600060612180606121417040106170401060604011406040106070103061401031707010317140103071601011616010116070101070701010907010110080102081401010913030111140101121201011311010114090102150801011511010116120102");
+        _bases[4] = Attribute("Odd Skull", 
+            "0000240600172407000607111706071107060301070701021406030116070102071401030916010111160101131404031513020109130102081101021210010411110302");
+        _bases[5] = Attribute("Holy Shield", 
+            "000024060019240500060613180606130806030113060301061805011318050106170401141704010616030115160301061402021614020206110103171101031108020809100602");
+        _bases[6] = Attribute("Crystal Ball", 
+            "000024060018240600060612180606120606040114060401061704011417040117140103170701030607010306140103071601011616010116070101070701011016040114150201081502010814010115140101161001040710010408080204140802040912060110130401100704021209020210110401");
+        _bases[7] = Attribute("Candle", 
+            "000024040019240500040815170407151618010115170101160401131504011208040214100401021204010211060101100701021207010211080101130402131317010110160301");
 
-        _totalBases = 4;
-
+        _totalBases = 8;
 
         // Initial effects
         _effects[0] = Attribute("Common", 
@@ -70,26 +83,27 @@ contract Items is ItemsInterface, ERC1155, Ownable {
         _totalEffects += names.length;
     }
 
-    function _afterTokenTransfer(address /*operator*/, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory /*data*/) internal override {
-        for(uint256 i = 0; i < ids.length; i++) {
-            _totalBalances[from][ids[i]] -= amounts[i];
-            _totalBalances[to][ids[i]] += amounts[i];
-        }
-    }
-
     function uri(uint256 id) public view virtual override returns (string memory) {
-        uint256 base = id % 100;
+        uint256 base = _items[id].svgHash % 100;
         require(base < _totalBases);
-        uint256 effect = (id / 100) - 100;
+        uint256 effect = (_items[id].svgHash / 100) - 100;
         require(effect < _totalEffects);
 
         string memory svgHeader = "<svg xmlns='http://www.w3.org/2000/svg' id='block-hack' preserveAspectRatio='xMinYMin meet' viewBox='0 0 24 24'><style>#block-hack{shape-rendering: crispedges;}</style>";
 
         return StringHelper.encodeMetadata(
-            string(abi.encodePacked(_bases[base].name, " (", _effects[effect].name, ")")),
+            _items[id].name,
             "Description", 
             string(abi.encodePacked(svgHeader, _effects[effect].value, StringHelper.hashToSvg(_bases[base].value), "</svg>")), 
             "Attributes"
         );
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function itemSupply(uint256 id) external view returns (uint256) {
+        return _totalSupplys[id];
     }
 }
--- scripts/uri.py
+++ scripts/uri.py
@@ -1,7 +1,7 @@
-from brownie import Cards, Characters, accounts
+from brownie import Cards, Characters, Items, accounts
 
 def main():
     cards = Cards.deploy(accounts[1], accounts[2], {"from": accounts[0]})
-    print(cards.uri(10103))
-
     characters = Characters.deploy(accounts[1], accounts[2], {"from": accounts[0]})
+    items = Items.deploy({"from": accounts[0]})
+    print(items.uri(10000))
